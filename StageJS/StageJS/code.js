b1254a21dad3f90d31c04b128db5bc7e09d94cac
tirouze <timothe.rouze@etu.udamail.fr>
2018-04-17 16:02:27 +0200

    "Buttons that trigger the right js script"

--- StageJS/StageJS/code.js
+++ StageJS/StageJS/code.js
@@ -4,10 +4,10 @@ img.src = "img/download.jpg"
 
 img.onload = function(){
     dessiner(this);
-    decreaseRed(this);
+    
 }
 
-function shadeGrey(img){
+function shadeGrey(){
     var canevasGris = document.getElementById('modify');
     var contextGris = canevasGris.getContext('2d');
     contextGris.drawImage(img, 0, 0);
@@ -22,7 +22,7 @@ function shadeGrey(img){
     contextGris.putImageData(imgData, 0, 0);
 }
 
-function invert(img){
+function invert(){
     var canvasInvert = document.getElementById('modify');
     var contextInvert = canvasInvert.getContext('2d');
     contextInvert.drawImage(img, 0, 0);
@@ -36,7 +36,8 @@ function invert(img){
     contextInvert.putImageData(imgData, 0, 0);
 }
 
-function decreaseRed(img){
+
+function decreaseRed(){
     var canvasDecreaseRed = document.getElementById('modify');
     var contextDecreaseRed = canvasDecreaseRed.getContext('2d');
     contextDecreaseRed.drawImage(img, 0, 0);
@@ -48,7 +49,7 @@ function decreaseRed(img){
     contextDecreaseRed.putImageData(imgData, 0, 0);
 }
 
-function decreaseGreen(img){
+function decreaseGreen(){
     var canvasDecreaseGreen = document.getElementById('modify');
     var contextDecreaseGreen = canvasDecreaseGreen.getContext('2d');
     contextDecreaseGreen.drawImage(img, 0, 0);
@@ -60,7 +61,7 @@ function decreaseGreen(img){
     contextDecreaseGreen.putImageData(imgData, 0, 0);
 }
 
-function decreaseBlue(img){
+function decreaseBlue(){
     var canvasDecreaseBlue = document.getElementById('modify');
     var contextDecreaseBlue = canvasDecreaseBlue.getContext('2d');
     contextDecreaseBlue.drawImage(img, 0, 0);
@@ -73,8 +74,11 @@ function decreaseBlue(img){
 }
 
 
-function dessiner(img){
+function dessiner(){
     var base = document.getElementById('base');
     var context_base = base.getContext('2d');
     context_base.drawImage(img, 0, 0);
+
+    
 }
+
