95832721e9ce85211dc9552113d51c07545a0aa7
noemiechev <noemiec21@hotmail.fr>
2018-04-18 11:37:17 +0100

    Add files via upload
    Correction bug debut avec les boucles imbriquees

--- StageJS/StageJS/5.1_CopyingPixel.js
+++ StageJS/StageJS/5.1_CopyingPixel.js
@@ -6,8 +6,7 @@ var img = new Image();
 img.src = "img/download.jpg"
 img.onload = function(){
     drawBase();
-    //ChangePixelColorNestedLoop(this);
-    lighten2(this);
+    mirrorVertial(this);
 
 }
 /**
@@ -15,10 +14,14 @@ img.onload = function(){
  */
 function drawBase() {
     var base = document.getElementById('base');
+    //normalise the caneva with the image
+    base.width = img.width;
+    base.height = img.height;
     var context_base = base.getContext('2d');
     context_base.drawImage(img, 0, 0);
     img.style.display = 'none';
 }
+
 /**
  * Method to get the indice of a pixel
  * @param x
@@ -28,7 +31,7 @@ function drawBase() {
  */
 function getPixel(x, y, width) {
     var colorIndice = y * (width * 4) + x * 4;
-    return [colorIndice, colorIndice+ 1, colorIndice+ 2, colorIndice+ 3];
+    return [colorIndice, colorIndice+1, colorIndice+2, colorIndice+3];
 }
 
 /**
@@ -38,46 +41,28 @@ function getPixel(x, y, width) {
 function ChangePixelColorNestedLoop(img){
     //init the canvas and the image
     var canevasModify = document.getElementById('modify');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
     var contextModify = canevasModify.getContext('2d');
     contextModify.drawImage(img, 0, 0);
     var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
     var data = imgData.data;
     //loop through the rows (y direction)
-    for (var row = 0; row < img.height; row ++) {
+    for (var row = 0; row < img.height; row++) {
         //loop through the cols (x direction)
-        for (var col=0; col<img.width; col ++){
+        for (var col = 0; col<img.width; col++){
             // get the pixel
-            var pixel= getPixel(col,row,img.width);
+            var pixel = getPixel(col, row, img.width);
             //do something to the color
-            data[pixel[0]]=0;//red
-            data[pixel[1]]=0;//green
-            data[pixel[2]]=0;//blue
+            data[pixel[0]] = 0; //red
+            data[pixel[1]] = 0; //green
+            data[pixel[2]] = 0; //blue
         }
     }
     contextModify.putImageData(imgData, 0, 0);
 }
-/**
- * Method to get the indice of a pixel
- * @param img
- * @constructor
- */
-function ChangePixelColor(img){
-    //init the canvas and the image
-    var canevasModify = document.getElementById('modify');
-    var contextModify = canevasModify.getContext('2d');
-    contextModify.drawImage(img, 0, 0);
-    img.style.display = 'none';
-    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
-    var data = imgData.data;
-    //loop through the pixel
-    for (var i = 0; i < data.length; i += 4){
-        //do something to the color
-        data[pixel[0]]=0;//red
-        data[pixel[1]]=0;//green
-        data[pixel[2]]=0;//blue
-    }
-    contextModify.putImageData(imgData, 0, 0);
-}
+
 /**
  * Method to ligthen an image with nested loop
  * @param img
@@ -85,50 +70,87 @@ function ChangePixelColor(img){
 function lighten2WithNestedLoop(img) {
     //init the canvas and the image
     var canevasModify = document.getElementById('modify');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
     var contextModify = canevasModify.getContext('2d');
     contextModify.drawImage(img, 0, 0);
     var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
     var data = imgData.data;
     //loop through the rows (y direction)
-    for (var row = 0; row < img.height; row ++) {
+    for (var row = 0; row < img.height; row++) {
         //loop through the cols (x direction)
-        for (var col=0; col<img.width; col ++){
+        for (var col = 0; col<img.width; col++){
             // get the pixel
-            var pixel= getPixel(col,row,img.width);
+            var pixel= getPixel(col, row, img.width);
             //do something to the color
             data[pixel[3]]*=0.5;
         }
     }
     contextModify.putImageData(imgData, 0, 0);
 }
-/**
- * Method to ligthen an image
- * @param img
- */
-function lighten2(img) {
+
+function mirrorVertial(img) {
+    //init the canvas and the image
     var canevasModify = document.getElementById('modify');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
     var contextModify = canevasModify.getContext('2d');
     contextModify.drawImage(img, 0, 0);
-    img.style.display = 'none';
     var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
     var data = imgData.data;
-    for (var i = 0; i < data.length; i += 4){
-            data[i+3]*=0.5;
+    //loop through the rows (y direction)
+    for (var row = 0; row < img.height; row++) {
+        //loop through the cols (x direction)
+        for (var col = 0; col<img.width/2; col++){
+            // get the pixel
+            var pixel = getPixel(col, row, img.width);
+            var target = getPixel(img.width-col, row, img.width);
+            //do something to the color
+            data[target[0]] = data[pixel[0]]; //red
+            data[target[1]] = data[pixel[1]]; //green
+            data[target[2]] = data[pixel[2]]; //blue
+            data[target[3]] = data[pixel[3]]; //alpha
+        }
     }
     contextModify.putImageData(imgData, 0, 0);
 }
 
-//5.2 Copying And Transforming Pictures
-/**function copyPicture(img) {
-    var source=img.data;
-    var newImage= new Image();
-
-    for (var row = 0; row < img.width; row ++) {
+/**
+ * Method to change the color of an image with nested loops
+ * @param img
+ */
+function mirrorHorizontal(img){
+    //init the canvas and the image
+    var canevasModify = document.getElementById('modify');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext('2d');
+    contextModify.drawImage(img, 0, 0);
+    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
+    var data = imgData.data;
+    canevasModify.height = img.height;
+    canevasModify.width = img.width;
+    //loop through the height (y direction)
+    console.log(img.width, " - ", img.height);
+    console.log(canevasModify.width, " - ", canevasModify.height);
+    for (var row = 0; row < img.height/2; row++) {
         //loop through the cols (x direction)
-        for (var col=0; col<img.height; col ++){
+        for (var col = 0; col<img.width; col++){
             // get the pixel
-            var pixel= getPixel(row,col,img.width);
-            newImage[]
+            var pixel = getPixel(col, row, img.width);
+            var target = getPixel(col, img.height-row, img.width);
+            //do something to the color
+            data[target[0]] = data[pixel[0]]; //red
+            data[target[1]] = data[pixel[1]]; //green
+            data[target[2]] = data[pixel[2]]; //blue
+            data[target[3]] = data[pixel[3]]; //alpha
         }
     }
-}*/
+    contextModify.putImageData(imgData, 0, 0);
+}
+
+
+//5.2 Copying And Transforming Pictures
