a0d001c79ffcd4b6b3d18832141612fee68c6ed0
TimRouze <timothe.rouze1@gmail.com>
2018-04-19 15:55:57 +0200

    Add files via upload

---
+++ StageJS/StageJS/StageJS/codeCh5.1.js
@@ -0,0 +1,327 @@
+/**
+ * Created by 1715844 on 17/04/2018.
+ */
+//Image Loadimg
+
+var img = document.getElementById("flower");
+window.onload = function(){
+    drawBase();
+    //mirrorVertial(this);
+
+}
+/**
+ * Method to draw the reference image
+ */
+function drawBase() {
+    var base = document.getElementById('baseFlower');
+    //normalise the caneva with the image
+    base.width = img.width;
+    base.height = img.height;
+    var context_base = base.getContext('2d');
+    context_base.drawImage(img, 0, 0);
+    img.style.display = 'none';
+}
+
+/**
+ * Method to get the indice of a pixel
+ * @param x
+ * @param y
+ * @param width of the image
+ * @returns {[the indice of the red componet of the pixel, same for green, same for blue, same for alpha]}
+ */
+function getPixel(x, y, width) {
+    var colorIndice = y * (width * 4) + x * 4;
+    return [colorIndice, colorIndice+1, colorIndice+2, colorIndice+3];
+}
+
+function ChangePixelColorNestedLoop(){
+    //init the canvas and the image
+    var canevasModify = document.getElementById('changePixelColor');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext('2d');
+    contextModify.drawImage(img, 0, 0);
+    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
+    var data = imgData.data;
+    //loop through the rows (y direction)
+    for (var row = 0; row < img.height; row++) {
+        //loop through the cols (x direction)
+        for (var col = 0; col<img.width; col++){
+            // get the pixel
+            var pixel = getPixel(col, row, img.width);
+            //do something to the color
+            data[pixel[0]] = 0; //red
+            data[pixel[1]] = 0; //green
+            data[pixel[2]] = 0; //blue
+        }   
+    }
+    contextModify.putImageData(imgData, 0, 0);
+    changePixelColorNestedLoopCode();
+}
+
+function lighten2WithNestedLoop(){
+    //init the canvas and the image
+    var canevasModify = document.getElementById('lighten2');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext('2d');
+    contextModify.drawImage(img, 0, 0);
+    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
+    var data = imgData.data;
+    //loop through the rows (y direction)
+    for (var row = 0; row < img.height; row++) {
+        //loop through the cols (x direction)
+        for (var col = 0; col<img.width; col++){
+            // get the pixel
+            var pixel= getPixel(col, row, img.width);
+            //do something to the color
+            data[pixel[3]]*=0.5;
+        }
+    }
+    contextModify.putImageData(imgData, 0, 0);
+    lighten2WithNestedLoopCode();
+}
+
+function mirrorVertial() {
+    //init the canvas and the image
+    var canevasModify = document.getElementById('mirrorVert');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext('2d');
+    contextModify.drawImage(img, 0, 0);
+    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
+    var data = imgData.data;
+    //loop through the rows (y direction)
+    for (var row = 0; row < img.height; row++) {
+        //loop through the cols (x direction)
+        for (var col = 0; col<img.width/2; col++){
+            // get the pixel
+            var pixel = getPixel(col, row, img.width);
+            var target = getPixel(img.width-col, row, img.width);
+            //do something to the color
+            data[target[0]] = data[pixel[0]]; //red
+            data[target[1]] = data[pixel[1]]; //green
+            data[target[2]] = data[pixel[2]]; //blue
+            data[target[3]] = data[pixel[3]]; //alpha
+        }
+    }
+    contextModify.putImageData(imgData, 0, 0);
+    mirrorVertialCode();
+}
+
+function mirrorHorizontalTopToBottom(){
+    //init the canvas and the image
+    var canevasModify = document.getElementById('mirrorHori');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext('2d');
+    contextModify.drawImage(img, 0, 0);
+    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
+    var data = imgData.data;
+    canevasModify.height = img.height;
+    canevasModify.width = img.width;
+    //loop through the height (y direction)
+    for (var row = 0; row < img.height/2; row++) {
+        //loop through the cols (x direction)
+        for (var col = 0; col<img.width; col++){
+            // get the pixel
+            var pixel = getPixel(col, row, img.width);
+            var target = getPixel(col, img.height-row, img.width);
+            //do something to the color
+            data[target[0]] = data[pixel[0]]; //red
+            data[target[1]] = data[pixel[1]]; //green
+            data[target[2]] = data[pixel[2]]; //blue
+            data[target[3]] = data[pixel[3]]; //alpha
+        }
+    }
+    contextModify.putImageData(imgData, 0, 0);
+    mirrorHorizontalCode();
+}
+
+function mirrorHorizontalBottomToTop(){
+    //init the canvas and the image
+    var canevasModify = document.getElementById('modify');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext('2d');
+    contextModify.drawImage(img, 0, 0);
+    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
+    var data = imgData.data;
+    canevasModify.height = img.height;
+    canevasModify.width = img.width;
+    //loop through the height (y direction)
+    for (var row = 0; row < img.height/2; row++) {
+        //loop through the cols (x direction)
+        for (var col = 0; col<img.width; col++){
+            // get the pixel
+            var pixel = getPixel(col, img.height-row, img.width);
+            var target = getPixel(col, row, img.width);
+            //do something to the color
+            data[target[0]] = data[pixel[0]]; //red
+            data[target[1]] = data[pixel[1]]; //green
+            data[target[2]] = data[pixel[2]]; //blue
+            data[target[3]] = data[pixel[3]]; //alpha
+        }
+    }
+    contextModify.putImageData(imgData, 0, 0);
+}
+
+function mirrorTemplePartVertical() {
+    var mirrorPoint = 768;
+    img.src = "img/temple.jpg";
+    //init the canvas and the image
+    var canevasModify = document.getElementById('modify');
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext('2d');
+    contextModify.drawImage(img, 0, 0);
+    var imgData = contextModify.getImageData(0, 0, canevasModify.width, canevasModify.height);
+    var data = imgData.data;
+    canevasModify.height = img.height;
+    canevasModify.width = img.width;
+    //loop through the height (y direction)
+    for (var row = 110; row < 310; row++) {
+        //loop through the cols (x direction)
+        for (var col = 1; col < 770; col++){
+            // get the pixel
+            var pixel = getPixel(mirrorPoint-col, row, img.width);
+            var target = getPixel(mirrorPoint+col, row, img.width);
+            //do something to the color
+            data[target[0]] = data[pixel[0]]; //red
+            data[target[1]] = data[pixel[1]]; //green
+            data[target[2]] = data[pixel[2]]; //blue
+            data[target[3]] = data[pixel[3]]; //alpha
+        }
+    }
+    contextModify.putImageData(imgData, 0, 0);
+}
+
+//5.2 Copying And Transforming Pictures
+
+function copyPicture() {
+    var base = document.getElementById('base');
+    //normalise the caneva with the image
+    base.width = img.width;
+    base.height = img.height;
+    var context_base = base.getContext('2d');
+    var source= context_base.getImageData(0, 0,base.width, base.height);
+    var canevasModify = document.getElementById("modify");
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext("2d");
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var target= contextModify.createImageData(img.width, img.height);
+    for (var row = 0; row < img.height; row ++) {
+        //loop through the cols (x direction)
+        for (var col=0; col<img.width; col ++){
+            // get the pixel
+            var pixel= getPixel(col,row,img.width);
+            target.data[pixel[0]]=source.data[pixel[0]];
+            target.data[pixel[1]]=source.data[pixel[1]];
+            target.data[pixel[2]]=source.data[pixel[2]];
+            target.data[pixel[3]]=source.data[pixel[3]];
+        }
+    }
+    contextModify.putImageData(target, 0, 0);
+}
+
+function copyPictureMidway() {
+    var base = document.getElementById('base');
+    //normalise the caneva with the image
+    base.width = img.width;
+    base.height = img.height;
+    var context_base = base.getContext('2d');
+    var source= context_base.getImageData(0, 0,base.width, base.height);
+    var canevasModify = document.getElementById("modify");
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext("2d");
+    //normalise the caneva with the image
+    canevasModify.width = 1000;
+    canevasModify.height = 1000;
+    var target= contextModify.createImageData(img.width, img.height);
+    for (var row = 0; row < img.height; row ++) {
+        //loop through the cols (x direction)
+        for (var col=0; col<img.width; col ++){
+            // get the pixel
+            var pixel= getPixel(col,row,img.width);
+            target.data[pixel[0]]=source.data[pixel[0]];
+            target.data[pixel[1]]=source.data[pixel[1]];
+            target.data[pixel[2]]=source.data[pixel[2]];
+            target.data[pixel[3]]=source.data[pixel[3]];
+        }
+    }
+    contextModify.putImageData(target, 100, 100);
+}
+
+function copyPictureSmallPart() {
+    var base = document.getElementById('base');
+    //normalise the caneva with the image
+    base.width = img.width;
+    base.height = img.height;
+    var context_base = base.getContext('2d');
+    var source= context_base.getImageData(0, 0,base.width, base.height);
+    var canevasModify = document.getElementById("modify");
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var contextModify = canevasModify.getContext("2d");
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var target= contextModify.createImageData(img.width, img.height);
+    for (var row = 0; row < 170; row ++) {
+        //loop through the cols (x direction)
+        for (var col=70; col<260; col ++){
+            // get the pixel
+            var pixel= getPixel(col,row,img.width);
+            target.data[pixel[0]]=source.data[pixel[0]];
+            target.data[pixel[1]]=source.data[pixel[1]];
+            target.data[pixel[2]]=source.data[pixel[2]];
+            target.data[pixel[3]]=source.data[pixel[3]];
+        }
+    }
+    contextModify.putImageData(target, 0, 0);
+}
+
+function copyAndLeftRotation() {
+    var base = document.getElementById('base');
+    var context_base = base.getContext('2d');
+    var source = context_base.getImageData(0, 0, base.width, base.height);
+    var canevasModify = document.getElementById("modify");
+    var contextModify = canevasModify.getContext("2d");
+    //normalise the caneva with the image
+    canevasModify.width = img.width;
+    canevasModify.height = img.height;
+    var target = contextModify.createImageData(img.width, img.height);
+    //loop through the cols (x direction)
+    for (var col = 0; col<img.width; col++){
+        //loop through the cols (x direction)
+        for (var row = 0; row < img.height; row++) {
+            // get the pixel
+            var pixel = getPixel(col, row, img.width);
+            var cible = getPixel(row, img.width-1-col, img.width);
+            target.data[cible[0]] = source.data[pixel[0]];
+            target.data[cible[1]] = source.data[pixel[1]];
+            target.data[cible[2]] = source.data[pixel[2]];
+            target.data[cible[3]] = source.data[pixel[3]];
+        }
+    }
+    contextModify.putImageData(target, 0, 0);
+}
+
+
+
+
+
+
