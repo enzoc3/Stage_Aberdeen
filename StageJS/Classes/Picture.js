6c8fd81e1bf3ee43de5f1a3b885084de169dfe56
Enzo Contini <enzo.contini@etu.univ-lyon1.fr>
2018-04-23 12:08:59 +0100

    Modif Picture.js and html

--- StageJS/Classes/Picture.js
+++ StageJS/Classes/Picture.js
@@ -1,3 +1,29 @@
-function Picture(){
-    /* AIMEZ VOUS LES UNS LES AUTRES COMME JE VOUS AI AIMES */
+var image=new Image();
+image.src="resources/download.jpg";
+
+function Picture(src){
+    this.source=src;
+    //document.getElementById('ladiv').innerHTML = '<canvas id="test"></canvas>';
+    
+    this.canvasdeTest= document.createElement("canvas");
+    var contextdeTest=canvasdeTest.getContext('2d');
+    contextdeTest.drawImage(src,0,0);
+    src.style.display='none';
+    this.height=src.height;
+    this.width=src.width;
+    this.arrayPixel=[];
+    for(var i=0;i<width*4;i++){
+        /* premier tableau */ 
+        for(var j=0;j<width;j++){
+            /*second tableau*/
+            tab[j] = [];
+            for(var k=0;k<height;k++){
+                tab[j][k]=tab[i];
+            }
+        }
+    }
 };
+
+var test = new Picture(image);
+
+console.log(test.height);
