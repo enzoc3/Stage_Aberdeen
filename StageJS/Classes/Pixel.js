ee8d62965a20c0d51a03598f35063cffd7cc71ee
tirouze <timothe.rouze@etu.udamail.fr>
2018-04-23 11:34:17 +0200

    Changement de strat

--- StageJS/Classes/Pixel.js
+++ StageJS/Classes/Pixel.js
@@ -1,10 +1,12 @@
-class Pixel{
-    constructor(r, g, b, a, x, y){
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
-        this.x = x;
-        this.y = y;
-    }
+function Pixel(r, g, b, a, x, y){
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+    this.x = x;
+    this.y = y;
 }
+
+var pix = new Pixel(10,10,10,0,0,0);
+
+console.log(pix);
