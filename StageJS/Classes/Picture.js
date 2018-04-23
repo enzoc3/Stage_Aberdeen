38ce783188cabb4744ddb851f8fa28524f5ef6cd
VINCENT MOREL (1715844) <1715844@rgu.ac.uk>
2018-04-23 16:33:11 +0100

    classe picture

--- StageJS/Classes/Picture.js
+++ StageJS/Classes/Picture.js
@@ -1,29 +1,50 @@
-var image=new Image();
-image.src="resources/download.jpg";
+function getImage(path) {
+    var image=new Image();
+    image.src=path;
+    return image;
+}
 
-function Picture(src){
-    this.source=src;
-    //document.getElementById('ladiv').innerHTML = '<canvas id="test"></canvas>';
-    
-    this.canvasdeTest= document.createElement("canvas");
-    var contextdeTest=canvasdeTest.getContext('2d');
-    contextdeTest.drawImage(src,0,0);
-    src.style.display='none';
-    this.height=src.height;
-    this.width=src.width;
-    this.arrayPixel=[];
-    for(var i=0;i<width*4;i++){
-        /* premier tableau */ 
-        for(var j=0;j<width;j++){
-            /*second tableau*/
-            tab[j] = [];
-            for(var k=0;k<height;k++){
-                tab[j][k]=tab[i];
-            }
-        }
-    }
+function getImageData(image) {
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute("width",image.width);
+    canvas.setAttribute("height",image.height);
+    document.body.appendChild(canvas);
+    canvas.style.display="none";
+    var context2d=canvas.getContext('2d');
+    context2d.drawImage(image,0,0);
+    return context2d.getImageData(0,0,image.width,image.height);
+}
+/*var image=new Image();
+image.src="resources/flower1.png";*/
+
+function getPixelData(x, y, width) {
+    var colorIndice = y * (width * 4) + x * 4;
+    return [colorIndice, colorIndice+1, colorIndice+2, colorIndice+3];
+}
+
+function createCanvas(image){
+    var canvas=document.createElement("canvas");
+    canvas.setAttribute("width",image.width);
+    canvas.setAttribute("height",image.height);
+    document.body.appendChild(canvas);
+    return canvas;
+}
+
+function Picture(path){
+    this.source=getImage(path);
+    this.height=this.source.height;
+    this.width=this.source.width;
+    this.data=getImageData(this.source);
+    this.canvas=createCanvas(this.source);
+    this.context=this.canvas.getContext('2d');
+    this.getPixel= function (x,y) {
+        return getPixelData(x,y,this.width);
+    };
+    this.display= function(){
+        this.context.putImageData(this.data,0,0);
+    };
+
 };
 
-var test = new Picture(image);
-
-console.log(test.height);
+var pic= new Picture("resources/flower1.png");
+pic.display()
