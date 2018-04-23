c5453b6ec6e1025416f53f2c89bde63dc48f4dc5
tirouze <timothe.rouze@etu.udamail.fr>
2018-04-23 11:20:33 +0200

    constructeur.

---
+++ StageJS/Classes/Pixel.js
@@ -0,0 +1,10 @@
+class Pixel{
+    constructor(r, g, b, a, x, y){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+        this.x = x;
+        this.y = y;
+    }
+}
