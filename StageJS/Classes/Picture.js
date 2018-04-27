6cc48253f05707d1ceb86c2bab0454a4a0509db5
VINCENT MOREL (1715844) <1715844@rgu.ac.uk>
2018-04-27 11:06:54 +0100

    chap 4 + set modifie

--- StageJS/Classes/Picture.js
+++ StageJS/Classes/Picture.js
@@ -1,81 +1,174 @@
-function wait(ms) {
+/**
+ * Return if the variable is correctly initialised
+ * @param {String} name - the name of the variable
+ * @param {int} nb - the value of the variable
+ * @param {int} min - the minimal value possible of the variable
+ * @param {int} max - the maximal value possible of the variable
+ * @returns {boolean} the answer
+ */
+function isCorrect(name, nb, min, max) {
+    var ok = false;
+    if (nb != null) {
+        if (nb >= min && nb <= max) {
+            ok = true;
+        }
+        else {
+            window.alert("Be carefull, the variable " + name + " must be between " + min + " and " + max + " !");
+        }
+    }
+    else {
+        window.alert("Be carefull, the variable " + name + " must be initialized !");
+    }
+    return ok;
+}
+
+/**
+ * Function used to waiting
+ * @param {int} ms - the time in ms to wait
+ */
+function wait(ms){
     var start = Date.now(),
         now = start;
     while (now - start < ms) {
         now = Date.now();
     }
 }
-function getImage(path) {
-    var image=new Image();
-    image.src=path;
-    var tab= getImageData(image);
-    return [image,tab];
-    /*return new Promise(function(resolve,reject)
-    {
-        image.onload = function(){
-            var tab= getImageData(this);
-            resolve([this, tab]);
-        };
-    }
-    );*/
+
+/**
+ * Return an image which have a source path and the data of this image
+ * @param {String} path - the path source of the image
+ * @returns {[Image, ImageData]} the image and its data
+ */
+function getImage(path){
+    var image = new Image();
+    image.src = path;
+    var tab = getImageData(image);
+    return [image, tab];
 }
 
-function getImageData(image) {
+/**
+ * Return the context of the data of an image
+ * @param {String} image - the image which have a path source
+ * @returns {ImageData} the context of the data of an image
+ */
+function getImageData(image){
     var canvas = document.createElement('canvas');
-    canvas.id='source';
-    canvas.setAttribute("width",image.width);
-    canvas.setAttribute("height",image.height);
+    canvas.id = 'source';
+    canvas.setAttribute("width", image.width);
+    canvas.setAttribute("height", image.height);
     document.body.appendChild(canvas);
-    canvas.style.display="none";
-    var context2d=canvas.getContext('2d');
-    context2d.drawImage(image,0,0);
-    return context2d.getImageData(0,0,image.width,image.height);
+    canvas.style.display = "none";
+    var context2d = canvas.getContext('2d');
+    context2d.drawImage(image, 0, 0);
+    return context2d.getImageData(0, 0, image.width, image.height);
 }
 
-function getPixelIndices(x, y, width) {
+/**
+ * Return the value of red, green, blue and alpha of the pixel
+ * @param {int} x - the coordinate x of the pixel in the image between 0 and width-1
+ * @param {int} y - the coordinate y of the pixel in the image between 0 and height-1
+ * @param {int} width - the width of the image
+ * @returns {[int, int, int, int]} the value of red, green, blue and alpha of the pixel
+ */
+function getPixelIndices(x, y, width){
     var colorIndice = y * (width * 4) + x * 4;
-    return [colorIndice ,colorIndice +1,colorIndice+2,colorIndice +3];
+    return [colorIndice, colorIndice+1, colorIndice+2, colorIndice+3];
 }
 
-
-function Picture(path,wait = true ){
-    if(wait== true){
+/**
+ * Class Picture which create a picture with a source path and a time of waiting
+ * @param {String} path - the path source of the image
+ * @param {boolean} wait - if is it is necessary to wait the download of the image
+ * @constructor
+ */
+function Picture(path, load = true){
+    if(load == true){
         wait(500);
     }
     var promise = getImage(path);
-    //thus=this;
-    this.source=promise[0];
-    this.height=this.source.height;
-    this.width=this.source.width;
-    this.imgData=promise[1];
-    this.canvas=document.getElementById('source');
-    this.context=this.canvas.getContext('2d');
+    this.source = promise[0];
+    this.height = this.source.height;
+    this.width = this.source.width;
+    this.imgData = promise[1];
+    this.canvas = document.getElementById('source');
+    this.context = this.canvas.getContext('2d');
 
-    this.getPixel= function (x,y) {
-        var colorIndice = y * (this.width * 4) + x * 4;
-        return new Pixel(this.imgData.data[colorIndice],this.imgData.data[colorIndice+1],this.imgData.data[colorIndice+2],this.imgData.data[colorIndice+3],x,y);
+    /**
+     * Return a new pixel with a value of red, green, blue and alpha and with coordinate x and y
+     * @param {int} x - the coordinate x of the pixel in the picture
+     * @param {int} y - the coordinate y of the pixel in the picture
+     * @returns {Pixel} the new pixel with all information
+     */
+    this.getPixel = function (x, y){
+        var colorIndice = y * (this.canvas.width * 4) + x * 4;
+        return new Pixel(this.imgData.data[colorIndice], this.imgData.data[colorIndice+1], this.imgData.data[colorIndice+2], this.imgData.data[colorIndice+3], x, y);
     };
-    this.setPixel=function(r,g,b,a,x,y){
-        var pixIndice=getPixelIndices(x,y,this.width);
-        this.imgData.data[pixIndice[0]]=r;
-        this.imgData.data[pixIndice[1]]=g;
-        this.imgData.data[pixIndice[2]]=b;
-        this.imgData.data[pixIndice[3]]=a;
+
+    /**
+     * Set the pixel with news values of red, green, blue, alpha, news coordinates of x and y
+     * @param {int} r - the new value of red of the pixel between 0 and 255
+     * @param {int} g - the new value of green of the pixel between 0 and 255
+     * @param {int} b - the new value of blue of the pixel between 0 and 255
+     * @param {int} a - the new value of alpha of the pixel between 0 and 255
+     * @param {int} x - the new coordinate x of the pixel between 0 and width-1
+     * @param {int} y - the new coordinate y of the pixel between 0 and height-1
+     */
+    this.setPixel = function(newPixel, x, y){
+        if (isCorrect("posX", x, 0, this.width)){
+            if (isCorrect("posY", y, 0, this.height)){
+                var pixIndice = getPixelIndices(x, y, this.canvas.width);
+                this.imgData.data[pixIndice[0]] = newPixel.red;
+                this.imgData.data[pixIndice[1]] = newPixel.green;
+                this.imgData.data[pixIndice[2]] = newPixel.blue;
+                this.imgData.data[pixIndice[3]] = newPixel.alpha;
+            }
+        }
     };
-    this.display= function(){
-        this.context.putImageData(this.imgData,0,0);
-        this.canvas.style.display="inline";
+    this.setCanvas=function(newCanvas, setSize = true){
+        c=document.getElementById(newCanvas);
+        this.canvas=c;
+        if(setSize == true){
+            this.setCanvasSize(this.width,this.height);
+        }
+        this.context=this.canvas.getContext('2d');
+    }
+    this.setCanvasSize=function (newWidth,newheight) {
+        this.canvas.setAttribute("width",newWidth);
+        this.canvas.setAttribute("height",newheight);
+    }
+    /**
+     * Display the picture in the canevas
+     */
+    this.display = function(){
+        this.context.putImageData(this.imgData, 0, 0);
+        this.canvas.style.display = "inline";
     };
-    this.redraw= function(){
+
+    /**
+     * Redraw the image in its canevas
+     */
+    this.redraw = function(){
         this.display()
+    };
+
+    /**
+     * Return all information about the picture
+     * @returns {string} all information about the picture
+     */
+    this.toString = function(){
+        return "Path source : " + this.source + "\nWidth : " + this.width + "\nHeight : " + this.height;
     };
 }
 
+/**
+ * Function used to load a bigger image which need more time to load
+ * @param {String} path - the path source of the image
+ * @returns {Promise} the load of the image
+ */
 function createPicture(path){
-    return new Promise(function(resolve,reject)
+    return new Promise(function(resolve, reject)
     {
-        var pic= new Picture(path,false);
+        var pic = new Picture(path, false);
         resolve(pic);
     });
 }
-
--- StageJS/Site/codeCh4.js
+++ StageJS/Site/codeCh4.js
@@ -1,141 +1,113 @@
 
-var imgCh4 = document.getElementById("nous");
-
 window.onload = function(){
-    dessiner();
-}
-
-function dessiner(){
-    var base = document.getElementById('base');
-    var context_base = base.getContext('2d');
-    context_base.drawImage(imgCh4, 0, 0);
+    //draw();
 }
 
 function decreaseRed(){
-    var canvasDecreaseRed = document.getElementById('decreaseRed');
-    canvasDecreaseRed.width = imgCh4.width;
-    canvasDecreaseRed.height = imgCh4.height;
-    var contextDecreaseRed = canvasDecreaseRed.getContext('2d');
-    contextDecreaseRed.drawImage(imgCh4, 0, 0);
-    var imgData = contextDecreaseRed.getImageData(0, 0, canvasDecreaseRed.width, canvasDecreaseRed.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i] = data[i]*0.5;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('decreaseRed');
+    var pixel= null;
+    console.log(imgCh4);
+    for(var x = 0; x < imgCh4.width; x ++){
+        for(var y = 0; y < imgCh4.height; y ++){
+            pixel= imgCh4.getPixel(x,y);
+            pixel.red*=0.5;
+            imgCh4.setPixel(pixel,x,y);
+        }
     }
-    contextDecreaseRed.putImageData(imgData, 0, 0);
+
+    imgCh4.display();
     decreaseRedCode();
 }
 
 function decreaseGreen(){
-    var canvasDecreaseGreen = document.getElementById('decreaseGreen');
-    canvasDecreaseGreen.width = imgCh4.width;
-    canvasDecreaseGreen.height = imgCh4.height;
-    var contextDecreaseGreen = canvasDecreaseGreen.getContext('2d');
-    contextDecreaseGreen.drawImage(imgCh4, 0, 0);
-    var imgData = contextDecreaseGreen.getImageData(0, 0, canvasDecreaseGreen.width, canvasDecreaseGreen.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i+1] = data[i+1]*0.5;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('decreaseGreen');
+    var pixel= null;
+    console.log(imgCh4);
+    for(var x = 0; x < imgCh4.width; x ++){
+        for(var y = 0; y < imgCh4.height; y ++){
+            pixel= imgCh4.getPixel(x,y);
+            pixel.green*=0.5;
+            imgCh4.setPixel(pixel,x,y);
+        }
     }
-    contextDecreaseGreen.putImageData(imgData, 0, 0);
+    imgCh4.display();
     decreaseGreenCode();
 }
 
 function decreaseBlue(){
-    var canvasDecreaseBlue = document.getElementById('decreaseBlue');
-    canvasDecreaseBlue.width = imgCh4.width;
-    canvasDecreaseBlue.height = imgCh4.height;
-    var contextDecreaseBlue = canvasDecreaseBlue.getContext('2d');
-    contextDecreaseBlue.drawImage(imgCh4, 0, 0);
-    var imgData = contextDecreaseBlue.getImageData(0, 0, canvasDecreaseBlue.width, canvasDecreaseBlue.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i+2] = data[i+2]*0.5;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('decreaseBlue');
+    var pixel= null;
+    console.log(imgCh4);
+    for(var x = 0; x < imgCh4.width; x ++){
+        for(var y = 0; y < imgCh4.height; y ++){
+            pixel= imgCh4.getPixel(x,y);
+            pixel.blue*=0.5;
+            imgCh4.setPixel(pixel,x,y);
+        }
     }
-    contextDecreaseBlue.putImageData(imgData, 0, 0);
+    imgCh4.display();
     decreaseBlueCode();
 }
 
 function whileDecreaseRed(){
-    var canvasWhileDecreaseRed = document.getElementById('decreaseRedWhile');
-    canvasWhileDecreaseRed.width = imgCh4.width;
-    canvasWhileDecreaseRed.height = imgCh4.height;
-    var contextWhileDecreaseRed = canvasWhileDecreaseRed.getContext('2d');
-    contextWhileDecreaseRed.drawImage(imgCh4, 0, 0);
-    var imgData = contextWhileDecreaseRed.getImageData(0, 0, canvasWhileDecreaseRed.width, canvasWhileDecreaseRed.height);
-    var data = imgData.data;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('decreaseRedWhile');
     var i=0;
-    while(i<data.length){
-        data[i] = data[i]*0.5;
+    while(i<imgCh4.imgData.data.length){
+        imgCh4.imgData.data[i] =imgCh4.imgData.data[i]*0.5;
         i+=4;
     }
-    contextWhileDecreaseRed.putImageData(imgData, 0, 0);
+    imgCh4.display();
     decreaseRedWhileCode();
 }
 
 function whileDecreaseGreen(){
-    var canvasWhileDecreaseGreen = document.getElementById('decreaseGreenWhile');
-    canvasWhileDecreaseGreen.width = imgCh4.width;
-    canvasWhileDecreaseGreen.height = imgCh4.height;
-    var contextWhileDecreaseGreen = canvasWhileDecreaseGreen.getContext('2d');
-    contextWhileDecreaseGreen.drawImage(imgCh4, 0, 0);
-    var imgData = contextWhileDecreaseGreen.getImageData(0, 0, canvasWhileDecreaseGreen.width, canvasWhileDecreaseGreen.height);
-    var data = imgData.data;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('decreaseGreenWhile');
     var i=1;
-    while(i<data.length){
-        data[i] = data[i]*0.5;
+    while(i<imgCh4.imgData.data.length){
+        imgCh4.imgData.data[i] =imgCh4.imgData.data[i]*0.5;
         i+=4;
     }
-    contextWhileDecreaseGreen.putImageData(imgData, 0, 0);
+    imgCh4.display();
     decreaseGreenWhileCode();
 }
 
 function whileDecreaseBlue(){
-    var canvasWhileDecreaseBlue = document.getElementById('decreaseBlueWhile');
-    canvasWhileDecreaseBlue.width = imgCh4.width;
-    canvasWhileDecreaseBlue.height = imgCh4.height;
-    var contextWhileDecreaseBlue = canvasWhileDecreaseBlue.getContext('2d');
-    contextWhileDecreaseBlue.drawImage(imgCh4, 0, 0);
-    var imgData = contextWhileDecreaseBlue.getImageData(0, 0, canvasWhileDecreaseBlue.width, canvasWhileDecreaseBlue.height);
-    var data = imgData.data;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('decreaseBlueWhile');
+    imgCh4.setCanvasSize(800,600);
     var i=2;
-    while(i<data.length){
-        data[i] = data[i]*0.5;
+    while(i<imgCh4.imgData.data.length){
+        imgCh4.imgData.data[i] =imgCh4.imgData.data[i]*0.5;
         i+=4;
     }
-    contextWhileDecreaseBlue.putImageData(imgData, 0, 0);
+    imgCh4.display();
     decreaseBlueWhileCode();
 }
 
 function increaseRed(){
-    var canvasIncreaseRed = document.getElementById('increaseRed');
-    canvasIncreaseRed.width = imgCh4.width;
-    canvasIncreaseRed.height = imgCh4.height;
-    var contextIncreaseRed = canvasIncreaseRed.getContext('2d');
-    contextIncreaseRed.drawImage(imgCh4, 0, 0);
-    var imgData = contextIncreaseRed.getImageData(0, 0, canvasIncreaseRed.width, canvasIncreaseRed.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i] = data[i] *5.5;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('increaseRed');
+    for(var i = 0; i < imgCh4.imgData.data.length; i += 4){
+        imgCh4.imgData.data[i] = imgCh4.imgData.data[i] *5.5;
     }
-    contextIncreaseRed.putImageData(imgData, 0, 0);
+    imgCh4.display();
     increaseRedCode();
 }
 
 function clearBlue(){
-    var canvasClearBlue = document.getElementById('clearBlue');
-    canvasClearBlue.width = imgCh4.width;
-    canvasClearBlue.height = imgCh4.height;
-    var contextClearBlue = canvasClearBlue.getContext('2d');
-    contextClearBlue.drawImage(imgCh4, 0, 0);
-    var imgData = contextClearBlue.getImageData(0, 0, canvasClearBlue.width, canvasClearBlue.height);
-    var data = imgData.data;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('clearBlue');
     var i=2;
-    while(i<data.length){
-        data[i] = 0;
+    while(i<imgCh4.imgData.data.length){
+        imgCh4.imgData.data[i] =0;
         i+=4;
     }
-    contextClearBlue.putImageData(imgData, 0, 0);
+    imgCh4.display();
     clearBlueCode();
 }
 
@@ -145,18 +117,13 @@ function sunset(){
 }
 
 function changeRed(form){
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('changeRed');
     var amount = form.value.value;
-    var canvasChangeRed = document.getElementById('changeRed');
-    canvasChangeRed.width = imgCh4.width;
-    canvasChangeRed.height = imgCh4.height;
-    var contextChangeRed = canvasChangeRed.getContext('2d');
-    contextChangeRed.drawImage(imgCh4, 0, 0);
-    var imgData = contextChangeRed.getImageData(0, 0, canvasChangeRed.width, canvasChangeRed.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i] = data[i]*amount;
+    for(var i = 0; i < imgCh4.imgData.data.length; i += 4){
+        imgCh4.imgData.data[i] = imgCh4.imgData.data[i]*amount;
     }
-    contextChangeRed.putImageData(imgData, 0, 0);
+    imgCh4.display();
     changeRedCode();
 }
 
@@ -164,109 +131,80 @@ function changeColors(form){
     var redAmount = form.valueRed.value;
     var greenAmount = form.valueGreen.value;
     var blueAmount = form.valueBlue.value;
-    var canvasChangeColors = document.getElementById('changeColors');
-    canvasChangeColors.width = imgCh4.width;
-    canvasChangeColors.height = imgCh4.height;
-    var contextChangeColors = canvasChangeColors.getContext('2d');
-    contextChangeColors.drawImage(imgCh4, 0, 0);
-    var imgData = contextChangeColors.getImageData(0, 0, canvasChangeColors.width, canvasChangeColors.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i] = data[i]*redAmount;
-        data[i+1] = data[i+1]*greenAmount;
-        data[i+2] = data[i+2]*blueAmount;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('changeColors');
+    for(var i = 0; i < imgCh4.imgData.data.length; i += 4){
+        imgCh4.imgData.data[i] = imgCh4.imgData.data[i]*redAmount;
+        imgCh4.imgData.data[i+1] = imgCh4.imgData.data[i+1]*greenAmount;
+        imgCh4.imgData.data[i+2] = imgCh4.imgData.data[i+2]*blueAmount;
     }
-    contextChangeColors.putImageData(imgData, 0, 0);
-    changeColorsCode();
+    imgCh4.display();
 }
 
 function lighten(){
-    var canvaslighten = document.getElementById('lighten');
-    canvaslighten.width = imgCh4.width;
-    canvaslighten.height = imgCh4.height;
-    var contextlighten = canvaslighten.getContext('2d');
-    contextlighten.drawImage(imgCh4, 0, 0);
-    var imgData = contextlighten.getImageData(0, 0, canvaslighten.width, canvaslighten.height);
-    var data = imgData.data;
-    for(var i = 3; i < data.length; i += 4){
-        data[i] = data[i]*3.3;
-        data[i+1] = data[i+1]*3.3;
-        data[i+2] = data[i+2]*3.3;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('lighten');
+    for(var i = 0; i < imgCh4.imgData.data.length; i += 4){
+        imgCh4.imgData.data[i] = imgCh4.imgData.data[i]*3.3;
+        imgCh4.imgData.data[i+1] = imgCh4.imgData.data[i+1]*3.3;
+        imgCh4.imgData.data[i+2] = imgCh4.imgData.data[i+2]*3.3;
     }
-    contextlighten.putImageData(imgData, 0, 0);
+    imgCh4.display();
     lightenCode();
 }
 
 function darken(){//TO DO IN HTML
-    var canvasdarken = document.getElementById('darken');
-    canvasdarken.width = imgCh4.width;
-    canvasdarken.height = imgCh4.height;
-    var contextdarken = canvasdarken.getContext('2d');
-    contextdarken.drawImage(imgCh4, 0, 0);
-    var imgData = contextdarken.getImageData(0, 0, canvasdarken.width, canvasdarken.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i] = data[i]*0.7;
-        data[i+1] = data[i+1]*0.7;
-        data[i+2] = data[i+2]*0.7;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('darken');
+    for(var i = 0; i < imgCh4.imgData.data.length; i += 4){
+        imgCh4.imgData.data[i] = imgCh4.imgData.data[i]*0.7;
+        imgCh4.imgData.data[i+1] = imgCh4.imgData.data[i+1]*0.7;
+        imgCh4.imgData.data[i+2] = imgCh4.imgData.data[i+2]*0.7;
     }
-    contextdarken.putImageData(imgData, 0, 0);
+    imgCh4.display();
 }
 
 function invert(){
-    var canvasInvert = document.getElementById('invert');
-    canvasInvert.width = imgCh4.width;
-    canvasInvert.height = imgCh4.height;
-    var contextInvert = canvasInvert.getContext('2d');
-    contextInvert.drawImage(imgCh4, 0, 0);
-    var imgData = contextInvert.getImageData(0, 0, canvasInvert.width, canvasInvert.height);
-    var data = imgData.data;
-    for(var i = 0; i < data.length; i += 4){
-        data[i] = 255 - data[i];
-        data[i + 1] = 255 - data[i + 1];
-        data[i + 2] = 255 - data[i + 2];
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('invert');
+    for(var i = 0; i < imgCh4.imgData.data.length; i += 4){
+        imgCh4.imgData.data[i] = 255 - imgCh4.imgData.data[i];
+        imgCh4.imgData.data[i + 1] = 255 - imgCh4.imgData.data[i + 1];
+        imgCh4.imgData.data[i + 2] = 255 - imgCh4.imgData.data[i + 2];
     }
-    contextInvert.putImageData(imgData, 0, 0);
+    imgCh4.display();
     InvertCode();
 }
 
 function shadeGrey(){
-    var canevasGris = document.getElementById('grey');
-    canevasGris.width = imgCh4.width;
-    canevasGris.height = imgCh4.height;
-    var contextGris = canevasGris.getContext('2d');
-    contextGris.drawImage(imgCh4, 0, 0);
-    var imgData = contextGris.getImageData(0, 0, canevasGris.width, canevasGris.height);
-    var data = imgData.data;
-    for (var i = 0; i < data.length; i += 4) {
-        var moy = (data[i] + data[i + 1] + data[i + 2]) / 3;
-        data[i] = moy;
-        data[i + 1] = moy;
-        data[i + 2] = moy;
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('grey');
+    for (var i = 0; i < imgCh4.imgData.data.length; i += 4) {
+        var moy = (imgCh4.imgData.data[i] + imgCh4.imgData.data[i + 1] + imgCh4.imgData.data[i + 2]) / 3;
+        imgCh4.imgData.data[i] = moy;
+        imgCh4.imgData.data[i + 1] = moy;
+        imgCh4.imgData.data[i + 2] = moy;
     }
-    contextGris.putImageData(imgData, 0, 0);
+    imgCh4.display();
     greyCode();
 }
 
 function betterShadeGrey(){
-    var canevasBetterShadeGrey = document.getElementById('grey');
-    canevasBetterShadeGrey.width = imgCh4.width;
-    canevasBetterShadeGrey.height = imgCh4.height;
-    var contextBetterShadeGrey = canevasBetterShadeGrey.getContext('2d');
-    contextBetterShadeGrey.drawImage(imgCh4, 0, 0);
-    var imgData = contextBetterShadeGrey.getImageData(0, 0, canevasBetterShadeGrey.width, canevasBetterShadeGrey.height);
-    var data = imgData.data;
-    redValue=greenValue=blueValue=0;
-    for (var i = 0; i < data.length; i += 4) {
-        redValue=data[i]*0.299;
-        greenValue=data[i+1]*0.587;
-        blueValue=data[i+2]*0.114;
-        luminance = redValue+greenValue+blueValue;
-        data[i] = luminance;
-        data[i + 1] = luminance;
-        data[i + 2] = luminance;       
+    var imgCh4 = new Picture("img/nous.jpg");
+    imgCh4.setCanvas('grey');
+    var redValue=0;
+    var greenValue=0;
+    var blueValue=0;
+    for (var i = 0; i < imgCh4.imgData.data.length; i += 4) {
+        redValue=imgCh4.imgData.data[i]*0.299;
+        greenValue=imgCh4.imgData.data[i+1]*0.587;
+        blueValue=imgCh4.imgData.data[i+2]*0.114;
+        var luminance = redValue+greenValue+blueValue;
+        imgCh4.imgData.data[i] = luminance;
+        imgCh4.imgData.data[i + 1] = luminance;
+        imgCh4.imgData.data[i + 2] = luminance;
     }
-    contextBetterShadeGrey.putImageData(imgData, 0, 0);
+    imgCh4.display();
     betterShadeGreyCode();
 }
 
